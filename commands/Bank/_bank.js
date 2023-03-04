aa024da9750accedec6ab4b20536d20ecce005e5
Bots.Business <git.sync@bots.business>
2023-03-04 06:16:51 +0000

    Bots.Business export for bot id: 818300

---
+++ commands/Bank/_bank.js
@@ -0,0 +1,11 @@
+/*CMD
+  command: /bank
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: Bank
+  answer: Bank
+  keyboard: 🏦Put 10$ to bank deposit, Put 100$ to deposit2,\n➕ Get all from bank deposit, Add % to bank deposit,\n❌ Cancel
+  aliases: 🏦 bank
+CMD*/
+
---
+++ commands/_start.js
@@ -0,0 +1,22 @@
+/*CMD
+  command: /start
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+This bot is demo for ResourcesLib in BB app.
+
+See demo chat:
+https://t.me/bbdemoexchange
+
+You can transfer resources to anybody
+
+Run command now:
+/balance
+  ANSWER
+  keyboard: 
+  aliases: 
+CMD*/
+
