0c92d11717a7678fa43d81b89700bb769acaa6d1
Bots.Business <git.sync@bots.business>
2023-03-05 04:04:36 +0000

    Bots.Business export for bot id: 818300

--- commands/_start.js
+++ commands/_start.js
@@ -1,22 +1,18 @@
 /*CMD
   command: /start
   help: 
-  need_reply: 
+  need_reply: false
   auto_retry_time: 
   folder: 
+  answer: hello, i am Elaina. i have finally embarked on my long-awaited journey where i will meet many people and learn their unique stories. Throughout my journey, i will explore the world at its fullest, experiencing both its bright and dark sides, and beginning my legendary tale.
 
-  <<ANSWER
-This bot is demo for ResourcesLib in BB app.
+  <<KEYBOARD
 
-See demo chat:
-https://t.me/bbdemoexchange
-
-You can transfer resources to anybody
-
-Run command now:
-/balance
-  ANSWER
-  keyboard: 
+  KEYBOARD
   aliases: 
 CMD*/
 
+//disclaimer messages and slash tip (MingZih)
+Bot.sendMessage("use the slash button menu to start using commands\n\n*disclaimer*\nthis bot is not a paid service and free for everyone so it will display some ads sometimes please ignore it, the ads is not published by me");
+
+// @MingZih
